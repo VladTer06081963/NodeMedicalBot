9c2edb3f688b073dc399106288700ab83c6423ab
Wlad <patrol200611@ukr.net>
2024-09-18 13:33:48 +0300

    update config.js

--- config.js
+++ config.js
@@ -1,25 +1,5 @@
 // config.js
-
-require('dotenv').config();  // Загружаем переменные окружения из .env
-
-// const config = {
-//     botToken: process.env.BOT_TOKEN || 'your-fallback-bot-token',
-//     notionToken: process.env.NOTION_TOKEN || 'your-fallback-notion-token',
-//     databaseId: process.env.NOTION_DATABASE_ID || 'your-fallback-database-id',
-//     timezone: process.env.TIMEZONE || 'Europe/Moscow',  // Добавляем временную зону
-// };
-
-// // Проверка на обязательные переменные окружения
-// if (!config.botToken || !config.notionToken || !config.databaseId) {
-//     throw new Error('Отсутствуют необходимые переменные окружения');
-// }
-
-// module.exports = config;
-
-
-
 require('dotenv').config();
-
 module.exports = {
   NOTION_TOKEN: process.env.NOTION_TOKEN,
   NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
